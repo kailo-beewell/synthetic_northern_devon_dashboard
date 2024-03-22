c2fe27583d50efb1f091dc74e8c95348b4c2d657
amyheather <a.heather2@exeter.ac.uk>
2024-03-22 17:15:55 +0000

    Created plotly map (much better performance)

--- pages/1_Standard survey by area2.py
+++ pages/1_Standard survey by area2.py
@@ -1,4 +1,3 @@
-import geopandas as gpd
 from kailo_beewell_dashboard.map import (
     area_intro, choose_topic)
 from kailo_beewell_dashboard.page_setup import (
@@ -6,6 +5,9 @@ from kailo_beewell_dashboard.page_setup import (
 from kailo_beewell_dashboard.reuse_text import caution_comparing
 import pandas as pd
 import streamlit as st
+
+import plotly.express as px
+import json
 
 ##########
 # Set-up #
@@ -18,16 +20,9 @@ page_setup('public')
 if 'scores_rag' not in st.session_state:
     st.session_state.scores_rag = pd.read_csv(
         'data/survey_data/standard_area_aggregate_scores_rag.csv')
-if 'shapes' not in st.session_state:
-    # Import shapefile
-    shp_nd = gpd.read_file('data/area_data/shapefile_nd/shp_nd.shp')
-    # Filter to required columns (helps map speed)
-    shp_nd = (shp_nd[['MSOA21NM', 'geometry']]
-              .rename(columns={'MSOA21NM': 'msoa'}))
-    # Simplify geometry (helps map speed)
-    shp_nd['geometry'] = shp_nd['geometry'].simplify(1)
-    # Save to session state
-    st.session_state.shapes = shp_nd
+if 'geojson_nd' not in st.session_state:
+    f = open('data/area_data/geojson/combined_nd.geojson')
+    st.session_state.geojson_nd = json.load(f)
 
 # As we play around this one, import it from session state
 df_scores = st.session_state.scores_rag
@@ -40,9 +35,29 @@ st.subheader('Results by MSOA')
 # Create selectbox and get chosen topic
 chosen_variable_lab = choose_topic(df_scores)
 
+# Filter to chosen topic then filter to only used column (helps map speed)
+chosen_result = df_scores[df_scores['variable_lab'] == chosen_variable_lab]
+msoa_rag = chosen_result[['msoa', 'mean']]
+
 #######
 # Map #
 #######
+
+fig = px.choropleth_mapbox(
+    msoa_rag,
+    geojson=st.session_state.geojson_nd,
+    locations='msoa',
+    color='mean',
+    featureidkey="properties.MSOA11NM",
+    color_continuous_scale="Viridis",
+    mapbox_style="carto-positron",
+    center={'lat': 50.955, 'lon': -4.075},
+    zoom=8,
+    labels={'val': 'value'})
+
+fig.update_layout(margin={'r': 0, 't': 0, 'l': 0, 'b': 0})
+
+st.plotly_chart(fig)
 
 # Add caveat for interpretation
 st.subheader('Comparing between areas')
