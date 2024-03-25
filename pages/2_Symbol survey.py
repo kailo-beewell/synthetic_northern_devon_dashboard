8cef43c36e5768d28d3693e5f96a3dc6fd6375cf
amyheather <a.heather2@exeter.ac.uk>
2024-03-25 14:19:38 +0000

    Initial commit - blank symbol survey and sample pages

---
+++ pages/2_Symbol survey.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from kailo_beewell_dashboard.page_setup import page_setup, page_footer
+
+page_setup('public')
+
+st.title('Symbol #BeeWell Survey')
+
+page_footer('schools in Northern Devon')
---
+++ pages/3_Who took part.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from kailo_beewell_dashboard.page_setup import page_setup, page_footer
+
+page_setup('public')
+
+st.title('Title')
+
+page_footer('schools in Northern Devon')
